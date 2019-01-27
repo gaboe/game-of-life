bd3582eb364bb74858683990a380c15470a09c8f
gaboe <gabrielecegi@gmail.com>
2019-01-27 01:52:03 +0100

    Add top neigthbours calculation

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -3,7 +3,12 @@ import React from "react";
 type Props = { cell: GridCell };
 
 const Cell = ({ cell }: Props) => (
-  <div className={cell.active ? "cell active" : "cell"}>&nbsp;</div>
+  <div
+    data-idc={`x:${cell.x};y:${cell.y}`}
+    className={cell.active ? "cell active" : "cell"}
+  >
+    &nbsp;
+  </div>
 );
 
 export { Cell };
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,19 +3,42 @@ import { Grid } from "./Grid";
 const getIndexedArray = (l: number) => Array.from(Array(l), (_, x) => x);
 
 const getCells = () =>
-  getIndexedArray(10).map(x =>
-    getIndexedArray(10).map(y => {
+  getIndexedArray(4).map(x =>
+    getIndexedArray(4).map(y => {
       return { active: Math.random() >= 0.5, x, y } as GridCell;
     })
   );
+
+const CTI = (cell: GridCell) => (cell.active ? 1 : 0);
 
 type State = { cells: GridCell[][]; isRunning: boolean };
 class Game extends React.Component<{}, State> {
   public state = { cells: getCells(), isRunning: false };
   public run = () => {
     console.log("running");
-    this.setState({ cells: getCells(), isRunning: true });
-    window.setTimeout(this.run, 1000);
+    const cells = this.state.cells;
+
+    const newCell = cells.map((row, rowIndex) => {
+      return row.map((col, colIndex) => {
+        const getTopCellsCount = () => {
+          const upperRow = cells[rowIndex + 1];
+          const onTop = CTI(upperRow[colIndex]);
+          const onTopLeft = colIndex > 0 ? CTI(upperRow[colIndex - 1]) : 0;
+          const onTopRight =
+            cells.length - 1 > colIndex ? CTI(upperRow[colIndex + 1]) : 0;
+
+          return onTop + onTopLeft + onTopRight;
+        };
+        const topN =
+          rowIndex > 1 && row.length - 1 > rowIndex ? getTopCellsCount() : 0;
+        console.log(col.x, col.y, topN);
+        return col;
+      });
+    });
+
+    this.setState({ cells: newCell, isRunning: false });
+    // this.setState({ cells: newCell, isRunning: true });
+    // window.setTimeout(this.run, 1000);
   };
 
   public render() {
